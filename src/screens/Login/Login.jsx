d05623a3a990048d8e6f4547b2bfee6fbc3c6f5d
Julio Sena - DTI <juliosouzasena@gmail.com>
2020-04-14 14:10:37 -0300

    insert email and password login

--- src/screens/Login/Login.jsx
+++ src/screens/Login/Login.jsx
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import TextField from '@material-ui/core/TextField';
+
+import './login.css';
 
 const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleChangePassword = (event) => {
+        setPassword(event.target.value);
+    }
+    
+    const handleChangeEmail = (event) => {
+        setEmail(event.target.value);
+    }
+
+    const onSubmitLogin = (event) => {
+        event.preventDefault();
+    }
+
     return (
-        <div>Login</div>
+        <div>
+            <form onSubmit={onSubmitLogin}>
+                <div className="box-login">
+                    <input value={email} onChange={handleChangeEmail}/>
+                    <input value={password} onChange={handleChangePassword} type="password" />
+                    <button type="submit">Entrar</button>
+                </div>
+            </form>
             
-    )
+        </div>   
+    );
 }
 
 export default Login;
